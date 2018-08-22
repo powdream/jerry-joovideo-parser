5a749eb32f8019eb512689d2fbffb8f40e21af2d
Kang Heejoon <heejoon.kang@linecorp.com>
2018-08-22 17:59:46 +0900

    Adds Program.host and Program.path.

--- src/main/kotlin/jerry/korvideo/model/Program.kt
+++ src/main/kotlin/jerry/korvideo/model/Program.kt
@@ -1,18 +1,28 @@
 package jerry.korvideo.model
 
+import java.util.Locale
+
 data class Program(
     val title: String,
     val link: String,
     val description: String,
     val optionalDescription: String? = null
 ) {
+    private val linkMatchResultGroupValues: List<String>? by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        REGEX_LINK.matchEntire(link)?.groupValues
+    }
+
+    val host: String?
+        get() = linkMatchResultGroupValues?.getOrNull(3)?.toLowerCase(Locale.ROOT)?.takeIf(String::isNotBlank)
+
+    val path: String
+        get() = linkMatchResultGroupValues?.getOrNull(4) ?: link
+
     val uniqueId: String
-        get() {
-            val groups = REGEX_UNIQUE_ID.matchEntire(link)?.groupValues
-            return groups?.last() ?: link
-        }
+        get() = linkMatchResultGroupValues?.lastOrNull() ?: link
 
     companion object {
-        private val REGEX_UNIQUE_ID = Regex("ViewLink[.]aspx[?]Num=(\\d+)", RegexOption.IGNORE_CASE)
+        private val REGEX_LINK =
+            Regex("(http(s)?://([^/]+).*)?(ViewLink[.]aspx[?]Num=(\\d+))", RegexOption.IGNORE_CASE)
     }
 }
--- src/test/kotlin/jerry/korvideo/model/ProgramTest.kt
+++ src/test/kotlin/jerry/korvideo/model/ProgramTest.kt
@@ -2,10 +2,11 @@ package jerry.korvideo.model
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class ProgramTest {
     @Test
-    fun getUniqueId() {
+    fun `getUniqueId - relative path`() {
         assertEquals(
             "2101700065911177226",
             Program(
@@ -22,6 +23,109 @@ class ProgramTest {
                 link = "viewlink.aspx?num=2101700065911177226",
                 description = "10회 12/20 Wed"
             ).uniqueId
+        )
+    }
+
+    @Test
+    fun `getUniqueId - absolute path`() {
+        assertEquals(
+            "2101700065911177226",
+            Program(
+                title = "로봇이 아니야",
+                link = "https://korvideo.com/ViewLink.aspx?Num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).uniqueId
+        )
+
+        assertEquals(
+            "2101700065911177226",
+            Program(
+                title = "로봇이 아니야",
+                link = "https://korvideo.com/viewlink.aspx?num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).uniqueId
+        )
+    }
+
+    @Test
+    fun `getHost - relative path`() {
+        assertNull(
+            Program(
+                title = "로봇이 아니야",
+                link = "ViewLink.aspx?Num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).host
+        )
+
+        assertNull(
+            Program(
+                title = "로봇이 아니야",
+                link = "viewlink.aspx?num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).host
+        )
+    }
+
+    @Test
+    fun `getHost - absolute path`() {
+        assertEquals(
+            "korvideo.com",
+            Program(
+                title = "로봇이 아니야",
+                link = "https://KORVIDEO.COM/ViewLink.aspx?Num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).host
+        )
+
+        assertEquals(
+            "korvideo.com",
+            Program(
+                title = "로봇이 아니야",
+                link = "https://KORVIDEO.COM/viewlink.aspx?num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).host
+        )
+    }
+
+    @Test
+    fun `getPath - relative path`() {
+        assertEquals(
+            "ViewLink.aspx?Num=2101700065911177226",
+            Program(
+                title = "로봇이 아니야",
+                link = "ViewLink.aspx?Num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).path
+        )
+
+        assertEquals(
+            "viewlink.aspx?num=2101700065911177226",
+            Program(
+                title = "로봇이 아니야",
+                link = "viewlink.aspx?num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).path
+        )
+    }
+
+    @Test
+    fun `getPath - absolute path`() {
+        assertEquals(
+            "ViewLink.aspx?Num=2101700065911177226",
+            Program(
+                title = "로봇이 아니야",
+                link = "https://KORVIDEO.COM/ViewLink.aspx?Num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).path
+        )
+
+        assertEquals(
+            "viewlink.aspx?num=2101700065911177226",
+            Program(
+                title = "로봇이 아니야",
+                link = "https://KORVIDEO.COM/viewlink.aspx?num=2101700065911177226",
+                description = "10회 12/20 Wed"
+            ).path
         )
     }
 }
