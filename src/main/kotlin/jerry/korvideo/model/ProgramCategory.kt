451ce9f3172f081e04a88ece7e3fd46828bb59c4
Heejoon Kang <haseo.hackers@gmail.com>
2019-03-18 18:46:04 +0900

    Bug fix

--- src/main/kotlin/jerry/korvideo/model/ProgramCategory.kt
+++ src/main/kotlin/jerry/korvideo/model/ProgramCategory.kt
@@ -7,7 +7,7 @@ enum class ProgramCategory(val tableId: String, val categoryName: String) {
     ENTERTAINMENT("tblEnt", "오락"),
     DOCUMENTARY("tblDoc", "다큐"),
     EVENT("tblEven", "시사"),
-    NEWS("tblNews", "뉴스 / 스포츠");
+    NEWS("tblNews", "뉴스,스포츠");
 
     companion object {
         val allValueList: List<ProgramCategory> = Collections.unmodifiableList(values().toList())
